f39c54189a5e078ebab2f1e370f1c5b6514fee18
Julian <yuffie069@gmail.com>
2023-07-29 22:23:45 -0300

    Agregamos las longitudes para el conversor de longitudes

---
+++ conversorDeMonedas/src/conversorDeMonedas/Longitudes.java
@@ -0,0 +1,10 @@
+package conversorDeMonedas;
+
+public class Longitudes {
+ 
+	public void convetirMetros(double medida) {}
+	public void convetirMillas(double medida) {}
+	public void convetirYardas(double medida) {}
+	public void convetirPies(double medida) {}
+	public void convetirMillasNauticas(double medida) {}
+}
---
+++ conversorDeMonedas/src/conversorDeMonedas/Milla.java
@@ -0,0 +1,26 @@
+package conversorDeMonedas;
+
+import javax.swing.JOptionPane;
+
+public class Milla extends Longitudes{
+	@override
+	public void convetirMetros(double medida) {
+		double valorConversion= medida*1609;
+		JOptionPane.showMessageDialog(null, "Serian " +valorConversion+ " Metros");
+	}
+	@override
+	public void convetirYardas(double medida) {
+		double valorConversion= medida*1760;
+		JOptionPane.showMessageDialog(null, "Serian " +valorConversion+ " Yardas");
+	}
+	@override
+	public void convetirPies(double medida) {
+		double valorConversion= medida*5280;
+		JOptionPane.showMessageDialog(null, "Serian " +valorConversion+ " Pies");
+	}
+	@override
+	public void convetirMillasNauticas(double medida) {
+		double valorConversion= medida/1.151;
+		JOptionPane.showMessageDialog(null, "Serian " +valorConversion+ " Millas Nauticas");
+	}
+}
